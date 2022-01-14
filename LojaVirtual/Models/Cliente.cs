a711f9c023c686ea259e6ab2eaec88e5f509bc0f
Daniel Lima <lldaniellimall@gmail.com>
2022-01-14 04:22:23 -0300

    Iniciando conexão com DB e preparando persistencia de clientes.

---
+++ LojaVirtual/Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string cpf { get; set; }
+        public string senha { get; set; }
+        public string sexo { get; set; }
+        public DateTime DataNascimento { get; set; }
+    }
+}
